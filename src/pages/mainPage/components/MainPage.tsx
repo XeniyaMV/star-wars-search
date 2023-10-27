172b10ff8363f4bfefffeae0d121b8ad6e2b8c22
XeniyaMV <mehnuchka.xd@gmail.com>
2023-10-26 19:24:33 -0700

    feat: add start screen

--- src/pages/mainPage/components/MainPage.tsx
+++ src/pages/mainPage/components/MainPage.tsx
@@ -1,11 +1,13 @@
 import { Component } from 'react';
 import Header from '../../../modules/header';
+import StartScreen from '../../../modules/startScreen';
 
 class MainPage extends Component {
   public render(): JSX.Element {
     return (
       <>
         <Header />
+        <StartScreen />
       </>
     );
   }
