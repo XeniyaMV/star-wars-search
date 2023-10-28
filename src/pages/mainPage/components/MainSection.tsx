2ba969a93079282a6c14a23560a5143eeeaadb8a
XeniyaMV <mehnuchka.xd@gmail.com>
2023-10-28 15:14:41 -0700

    refactor: remove example response

--- src/pages/mainPage/components/MainSection.tsx
+++ src/pages/mainPage/components/MainSection.tsx
@@ -1,40 +1,6 @@
 import { Component } from 'react';
 import SearchForm from '../../../modules/searchForm';
 import CharacterCards from '../../../modules/characterCards';
-import { CardInfoResponse } from '../../../types';
-
-const cardInfoExample: CardInfoResponse[] = [
-  {
-    name: 'Luke Skywalker',
-    height: '172',
-    mass: '77',
-    hair_color: 'blond',
-    skin_color: 'fair',
-    eye_color: 'blue',
-    birth_year: '19BBY',
-    gender: 'male',
-  },
-  {
-    name: 'Luke Skywalker',
-    height: '172',
-    mass: '77',
-    hair_color: 'blond',
-    skin_color: 'fair',
-    eye_color: 'blue',
-    birth_year: '19BBY',
-    gender: 'male',
-  },
-  {
-    name: 'Luke Skywalker',
-    height: '172',
-    mass: '77',
-    hair_color: 'blond',
-    skin_color: 'fair',
-    eye_color: 'blue',
-    birth_year: '19BBY',
-    gender: 'male',
-  },
-];
 
 class MainSection extends Component {
   public render(): JSX.Element {
@@ -44,7 +10,7 @@ class MainSection extends Component {
           <section className="search">
             <SearchForm submitTitle="Search" inputPlaceholder="Enter a Star Wars character" />
           </section>
-          <CharacterCards cardInfos={cardInfoExample} />
+          <CharacterCards cardInfos={[]} />
         </div>
       </main>
     );
