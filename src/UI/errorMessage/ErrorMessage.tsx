937542eac43c85dab1a9c2c12958528a1fcd2fbb
XeniyaMV <mehnuchka.xd@gmail.com>
2023-11-02 20:31:14 -0700

    refactor: switch ErrorMessage component from class to function

--- src/UI/errorMessage/ErrorMessage.tsx
+++ src/UI/errorMessage/ErrorMessage.tsx
@@ -1,18 +1,15 @@
-import { Component } from 'react';
 import getFullClassName from '../../helpers/getFullClassName';
 import { ErrorMessageProps } from '../../types';
 import alarmLight from '../../assets/alarm-light.svg';
 
-class ErrorMessage extends Component<ErrorMessageProps> {
-  private fullClassName = getFullClassName('error', this.props.additionalClassName);
-  public render(): JSX.Element {
-    return (
-      <div className={this.fullClassName}>
-        <img className="error__icon" src={alarmLight} alt="alarm icon" />
-        <span className="error__message">{this.props.errorMessage}</span>
-      </div>
-    );
-  }
-}
+const ErrorMessage = (props: ErrorMessageProps): JSX.Element => {
+  const fullClassName = getFullClassName('error', props.additionalClassName);
+  return (
+    <div className={fullClassName}>
+      <img className="error__icon" src={alarmLight} alt="alarm icon" />
+      <span className="error__message">{props.errorMessage}</span>
+    </div>
+  );
+};
 
 export default ErrorMessage;
