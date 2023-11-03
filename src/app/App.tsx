cd5af02b13c32fa7a7499a8a7d28878939d84a67
XeniyaMV <mehnuchka.xd@gmail.com>
2023-11-03 00:00:32 -0700

    refactor: switch App component from class to function

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,11 @@
-import { Component } from 'react';
 import MainPage from '../pages/mainPage/components/MainPage';
 
-class App extends Component {
-  public render(): JSX.Element {
-    return (
-      <>
-        <MainPage />
-      </>
-    );
-  }
-}
+const App = (): JSX.Element => {
+  return (
+    <>
+      <MainPage />
+    </>
+  );
+};
 
 export default App;
