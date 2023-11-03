dfc927e31c670e21f4b980fc8b3ab13291f97e22
XeniyaMV <mehnuchka.xd@gmail.com>
2023-11-02 23:56:06 -0700

    refactor: switch MainPage component from class to function

--- src/pages/mainPage/components/MainPage.tsx
+++ src/pages/mainPage/components/MainPage.tsx
@@ -1,18 +1,15 @@
-import { Component } from 'react';
 import Header from '../../../modules/header';
 import StartScreen from '../../../modules/startScreen';
 import MainSection from './MainSection';
 
-class MainPage extends Component {
-  public render(): JSX.Element {
-    return (
-      <>
-        <Header />
-        <StartScreen />
-        <MainSection />
-      </>
-    );
-  }
-}
+const MainPage = (): JSX.Element => {
+  return (
+    <>
+      <Header />
+      <StartScreen />
+      <MainSection />
+    </>
+  );
+};
 
 export default MainPage;
