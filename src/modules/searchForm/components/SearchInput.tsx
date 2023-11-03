8e30541bb9daf8bde96dde6b8cfe89a6173f6dc5
XeniyaMV <mehnuchka.xd@gmail.com>
2023-11-02 20:58:34 -0700

    refactor: switch SearchInput component from class to function

--- src/modules/searchForm/components/SearchInput.tsx
+++ src/modules/searchForm/components/SearchInput.tsx
@@ -1,22 +1,19 @@
-import { Component } from 'react';
 import { SearchInputProps } from '../../../types';
 import searchIcon from '../../../assets/search.svg';
 
-class SearchInput extends Component<SearchInputProps> {
-  public render(): JSX.Element {
-    return (
-      <div className="search-form__input">
-        <img className="search-form__icon" src={searchIcon} alt="search icon" />
-        <input
-          type="text"
-          className="search-form__text-field"
-          placeholder={this.props.inputPlaceholder || 'Enter'}
-          value={this.props.value}
-          onChange={(event): void => this.props.setValue(event)}
-        />
-      </div>
-    );
-  }
-}
+const SearchInput = (props: SearchInputProps): JSX.Element => {
+  return (
+    <div className="search-form__input">
+      <img className="search-form__icon" src={searchIcon} alt="search icon" />
+      <input
+        type="text"
+        className="search-form__text-field"
+        placeholder={props.inputPlaceholder || 'Enter'}
+        value={props.value}
+        onChange={(event): void => props.setValue(event)}
+      />
+    </div>
+  );
+};
 
 export default SearchInput;
