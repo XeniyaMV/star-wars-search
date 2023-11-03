47d7d36e48de2b3359973d39898b76f76ef2b0b6
XeniyaMV <mehnuchka.xd@gmail.com>
2023-11-02 23:52:34 -0700

    refactor: switch MainSection component from class to function

--- src/pages/mainPage/components/MainSection.tsx
+++ src/pages/mainPage/components/MainSection.tsx
@@ -1,48 +1,30 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import SearchForm from '../../../modules/searchForm';
 import CharacterCards from '../../../modules/characterCards';
 import ErrorButton from './ErrorButton';
-import { Props, CardInfo } from '../../../types';
+import { CardInfo } from '../../../types';
 
-class MainSection extends Component {
-  public state = {
-    cardInfos: [],
-    loader: false,
-  };
+const MainSection = (): JSX.Element => {
+  const [cardInfos, setCardInfos] = useState<CardInfo[]>([]);
+  const [loader, setLoader] = useState(false);
 
-  constructor(props: Props) {
-    super(props);
-    this.setCardInfos = this.setCardInfos.bind(this);
-    this.setLoader = this.setLoader.bind(this);
-  }
-
-  private setCardInfos(cardInfos: CardInfo[]): void {
-    this.setState({ cardInfos });
-  }
-
-  private setLoader(loader: boolean): void {
-    this.setState({ loader });
-  }
-
-  public render(): JSX.Element {
-    return (
-      <main className="main">
-        <div className="container main__wrapper">
-          <ErrorButton />
-          <section className="search">
-            <SearchForm
-              submitTitle="Search"
-              inputPlaceholder="Enter a Star Wars character"
-              loader={this.state.loader}
-              setCardInfos={this.setCardInfos}
-              setLoader={this.setLoader}
-            />
-          </section>
-          <CharacterCards cardInfos={this.state.cardInfos} loader={this.state.loader} />
-        </div>
-      </main>
-    );
-  }
-}
+  return (
+    <main className="main">
+      <div className="container main__wrapper">
+        <ErrorButton />
+        <section className="search">
+          <SearchForm
+            submitTitle="Search"
+            inputPlaceholder="Enter a Star Wars character"
+            loader={loader}
+            setCardInfos={setCardInfos}
+            setLoader={setLoader}
+          />
+        </section>
+        <CharacterCards cardInfos={cardInfos} loader={loader} />
+      </div>
+    </main>
+  );
+};
 
 export default MainSection;
