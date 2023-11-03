62a808c48644cfaaad8856871c4cd899de26f3ca
XeniyaMV <mehnuchka.xd@gmail.com>
2023-11-02 23:18:57 -0700

    refactor: switch SearchForm component from class to function

--- src/modules/searchForm/components/SearchForm.tsx
+++ src/modules/searchForm/components/SearchForm.tsx
@@ -1,6 +1,4 @@
-import { Component } from 'react';
-import { FormEvent } from 'react';
-import { ChangeEvent } from 'react';
+import { FormEvent, ChangeEvent, useState, useEffect } from 'react';
 import { SearchFormProps } from '../../../types';
 import getSearchResult from '../api/getSearchResult';
 import apiBase from '../constants/apiBase';
@@ -8,64 +6,54 @@ import transformResponseToCardInfo from '../helpers/transformResponseToCardInfo'
 import getFullClassName from '../../../helpers/getFullClassName';
 import SearchInput from './SearchInput';
 
-class SearchForm extends Component<SearchFormProps> {
-  public state = {
-    searchTerm: localStorage.getItem('searchTerm') || '',
+const SearchForm = (props: SearchFormProps): JSX.Element => {
+  const [searchTerm, setSearchTerm] = useState(localStorage.getItem('searchTerm') || '');
+  const fullClassName = getFullClassName('search-form', props.additionalClassName);
+
+  useEffect((): void => {
+    const fetchData = async (): Promise<void> => {
+      if (!props.loader) {
+        if (props.setLoader) props.setLoader(true);
+        const result = await getSearchResult(apiBase.baseUrl, apiBase.path, searchTerm);
+        if (props.setCardInfos) props.setCardInfos(transformResponseToCardInfo(result));
+        if (props.setLoader) props.setLoader(false);
+      }
+    };
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    event.preventDefault();
+    setSearchTerm(event.target.value);
   };
 
-  private fullClassName = getFullClassName('search-form', this.props.additionalClassName);
-
-  constructor(props: SearchFormProps) {
-    super(props);
-    this.handleInputChange = this.handleInputChange.bind(this);
-  }
-
-  private handleInputChange(event: ChangeEvent<HTMLInputElement>): void {
-    event.preventDefault();
-    this.setState({ searchTerm: event.target.value });
-  }
-
-  private async handleSubmit(event: FormEvent<HTMLFormElement>): Promise<void> {
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault();
 
-    if (!this.props.loader) {
-      if (this.props.setLoader) this.props.setLoader(true);
-      const search = this.state.searchTerm.trim();
-      this.setState({ searchTerm: search });
+    if (!props.loader) {
+      if (props.setLoader) props.setLoader(true);
+      const search = searchTerm.trim();
+      setSearchTerm(search);
       const result = await getSearchResult(apiBase.baseUrl, apiBase.path, search);
       localStorage.setItem('searchTerm', search);
-      if (this.props.setCardInfos) this.props.setCardInfos(transformResponseToCardInfo(result));
-      if (this.props.setLoader) this.props.setLoader(false);
+      if (props.setCardInfos) props.setCardInfos(transformResponseToCardInfo(result));
+      if (props.setLoader) props.setLoader(false);
     }
-  }
+  };
 
-  public async componentDidMount(): Promise<void> {
-    if (!this.props.loader) {
-      if (this.props.setLoader) this.props.setLoader(true);
-      const result = await getSearchResult(apiBase.baseUrl, apiBase.path, this.state.searchTerm);
-      if (this.props.setCardInfos) this.props.setCardInfos(transformResponseToCardInfo(result));
-      if (this.props.setLoader) this.props.setLoader(false);
-    }
-  }
-
-  public render(): JSX.Element {
-    return (
-      <form className={this.fullClassName} onSubmit={(event): Promise<void> => this.handleSubmit(event)}>
-        <SearchInput
-          value={this.state.searchTerm}
-          setValue={this.handleInputChange}
-          inputPlaceholder={this.props.inputPlaceholder}
-        />
-        <button
-          className={`button search-form__submit ${this.props.loader ? 'button_disabled' : ''}`}
-          type="submit"
-          disabled={this.props.loader}
-        >
-          {this.props.submitTitle}
-        </button>
-      </form>
-    );
-  }
-}
+  return (
+    <form className={fullClassName} onSubmit={(event): Promise<void> => handleSubmit(event)}>
+      <SearchInput value={searchTerm} setValue={handleInputChange} inputPlaceholder={props.inputPlaceholder} />
+      <button
+        className={`button search-form__submit ${props.loader ? 'button_disabled' : ''}`}
+        type="submit"
+        disabled={props.loader}
+      >
+        {props.submitTitle}
+      </button>
+    </form>
+  );
+};
 
 export default SearchForm;
