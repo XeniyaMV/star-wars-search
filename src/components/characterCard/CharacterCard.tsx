9f0dcbcc72a4356dc873e7c23eb174b469dcd6e3
XeniyaMV <mehnuchka.xd@gmail.com>
2023-11-02 20:45:34 -0700

    refactor: switch CharacterCard component from class to function

--- src/components/characterCard/CharacterCard.tsx
+++ src/components/characterCard/CharacterCard.tsx
@@ -1,26 +1,23 @@
-import { Component } from 'react';
 import { CharacterCardProps } from '../../types';
 import getFullClassName from '../../helpers/getFullClassName';
 import cardImg from '../../assets/card-picture.jpg';
 
-class CharacterCard extends Component<CharacterCardProps> {
-  private fullClassName = getFullClassName('character-card', this.props.additionalClassName);
+const CharacterCard = (props: CharacterCardProps): JSX.Element => {
+  const fullClassName = getFullClassName('character-card', props.additionalClassName);
 
-  public render(): JSX.Element {
-    return (
-      <div className={this.fullClassName}>
-        <ul className="list character-card__info">
-          {Object.entries(this.props.cardInfo).map((item) => (
-            <li key={item[0].concat(`: ${item[1]}`)} className="list__item">
-              <span className="character-card__info-name plain-text_highlight">{item[0].replace('_', ' ')}:</span>&nbsp;
-              {item[1]}
-            </li>
-          ))}
-        </ul>
-        <img className="character-card__image" src={cardImg} alt="card image" />
-      </div>
-    );
-  }
-}
+  return (
+    <div className={fullClassName}>
+      <ul className="list character-card__info">
+        {Object.entries(props.cardInfo).map((item) => (
+          <li key={item[0].concat(`: ${item[1]}`)} className="list__item">
+            <span className="character-card__info-name plain-text_highlight">{item[0].replace('_', ' ')}:</span>&nbsp;
+            {item[1]}
+          </li>
+        ))}
+      </ul>
+      <img className="character-card__image" src={cardImg} alt="card image" />
+    </div>
+  );
+};
 
 export default CharacterCard;
