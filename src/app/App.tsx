eb29f05a56890927d19f92bb79aa76dd64c4e16d
XeniyaMV <mehnuchka.xd@gmail.com>
2023-11-03 20:24:55 -0700

    feat: add error page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import RootLayout from './RootLayout';
 import MainPage from '../pages/mainPage/components/MainPage';
+import ErrorPage from '../pages/errorPage/components/ErrorPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
+    <Route path="/" element={<RootLayout />} errorElement={<ErrorPage />}>
       <Route index element={<MainPage />} />
     </Route>
   )
